c0daa7f2f525e08db8e8fbe27d1b165336722943
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-11-24 17:24:42 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+import pandas as pd
 sys.path.insert(0, 'src')
 from features import features_labels
 
@@ -16,13 +17,14 @@ def main(targets):
             
             # make the data target
             file_names, file_labels, new_df = features_labels(**feature_cfg)
+            new_df.to_csv("test/output/test_output.csv")
             print("The associated file names are: ") 
             print(file_names)
             print("The associated file labels are: ") 
             print(file_labels)
             print("Created the new features!")
             print(new_df)
-    return new_df
+    return
 
 if __name__=='__main__':
     #run via:
